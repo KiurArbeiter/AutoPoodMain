d5a87f979b33a5eba8e5d894304f335a802eb529
Opilane TTHK <opilane@tthk.ee>
2024-08-28 14:29:05 +0300

    Create PlaneModel.cs

---
+++ Autopood/Models/PlaneModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Autopood.Models
+{
+    public class PlaneModel
+    {
+        public int PlaneId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public string Register { get; set; }
+        public int SerialNumber { get; set; }
+        public string Engine { get; set; }
+        public string Propeller { get; set; }
+        public int TotalTime { get; set; }
+        public int Seats { get; set; }
+        public string Inspection { get; set; }
+        public bool InspectionResults { get; set; }
+    }
+}
